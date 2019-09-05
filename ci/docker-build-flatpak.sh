8d328699b306c1f8ba8e09201cec5e4151b91f86
Alec Leamas <leamas.alec@gmail.com>
2019-09-05 21:08:59 +0200

    build: trying machine for flatpak.

--- ci/docker-build-flatpak.sh
+++ ci/docker-build-flatpak.sh
@@ -6,20 +6,21 @@ cd $(dirname $(readlink -fn $0))
 #
 set -xe
 
-if [-n "$TRAVIS" ]; then
-    cd /opencpn-ci
+if [ -n "$TRAVIS" ]; then
+    TOPDIR=/opencpn-ci
 fi
 
-if [ "$CIRCLECI" ]; then
-   cd /root/project
+if [ -n "$CIRCLECI" ]; then
+   TOPDIR=/root/project
 fi
 
-su -c "dnf install -y sudo cmake gcc-c++ flatpak-builder flatpak make tar"
+su -c "dnf install -q -y sudo cmake gcc-c++ flatpak-builder flatpak make"
 flatpak remote-add --user --if-not-exists flathub \
     https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --user  -y \
         http://opencpn.duckdns.org/opencpn/opencpn.flatpakref
 flatpak install --user -y  flathub org.freedesktop.Sdk//18.08 
+cd $TOPDIR
 rm -rf build && mkdir build && cd build
 cmake -DOCPN_FLATPAK=ON ..
 make flatpak-build
